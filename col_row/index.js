1df0c6452f73519587c7bc9c4a5ff98fa86402c8
Kujani Gergo <kujani.gergo@BOLYAI.LOCAL>
2026-04-24 13:06:13 +0200

    a

--- col_row/index.js
+++ col_row/index.js
@@ -4,7 +4,7 @@ import { AuthorManager } from "./manager.js";
 import { NavigationBar } from "./navbar.js"
 import { TableView } from "./table.js";
 
-const formFields = [{
+const formFields = [{ // létrehozunk egy formfield listát ami alapján példányosítja a form controller a pédányokat
     id: 'author',
     label: 'Név',
     name: 'author'
@@ -20,7 +20,7 @@ const formFields = [{
     name: 'concept'
 }]
 
-const headerArray = ['Szerző', 'Mű', 'Fogalom']
+const headerArray = ['Szerző', 'Mű', 'Fogalom'] // létreozunk egy header listát
 
 /**
  * @typedef {{id: number, author?: string, work?: string, concept?: string}} AuthorType
@@ -28,23 +28,24 @@ const headerArray = ['Szerző', 'Mű', 'Fogalom']
  */
 
 
-const navbar = new NavigationBar();
-navbar.appendTo(document.body);
+const navbar = new NavigationBar(); //pédányosítjuk a managgert 
+navbar.appendTo(document.body); //példányosítjuk a nabart
  
-const manager = new AuthorManager();
+const manager = new AuthorManager(); //hozzáfűzzük a navbart a doc body hoz
 
 
-const tableView = new TableView("table",headerArray, manager);
-tableView.appendTo(document.body);
-navbar.addViewElement("tablazat",tableView)
+const tableView = new TableView("table",headerArray, manager);  //pédányosítjuk a tablet
+tableView.appendTo(document.body);//hozzáfűzzük a tablet a dpcument bodhoz
+navbar.addViewElement("tablazat",tableView) 
 
 const formView = new FormView("Tableform",formFields,manager);
 formView.appendTo(document.body);
 navbar.addViewElement("form",formView);
 
-navbar.activate("table");
+navbar.activate("table"); //meghívjuk a navbar activate metódusát
 
-const importExport = new ImportView("importexport",manager);
-importExport.appendTo(document.body);
-navbar.addViewElement("import/export",importExport)
+
+const importExport = new ImportView("importexport",manager); //hozzáfűzzük az importexportot a navbarhoz
+importExport.appendTo(document.body); //hozzáfűzzük a formcontrollert a navbarhoz
+navbar.addViewElement("import/export",importExport) //hozzáadjuk az importexportot a navbarhoz
 manager.getAllElement();
